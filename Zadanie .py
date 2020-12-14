4d6f7fc3c283174cc7c376f6b439483a30c38755
Lezakk <wiktor.zak94@gmail.com>
2020-12-14 22:54:46 +0100

    Add exception handling while connectnig db, some improvements

--- Zadanie .py
+++ Zadanie .py
@@ -1,18 +1,18 @@
 import sqlite3
 import csv
 
-conn = sqlite3.connect('TracksDB.db')
+try:
+    conn = sqlite3.connect('TracksDB.db')
+except sqlite3.Error:
+    print("Database connection failed.")
+
 
 cursor = conn.cursor()
 
 cursor.execute('''CREATE TABLE IF NOT EXISTS Music
     (performaceId INTEGER, trackId VARCHAR(255), artistName VARCHAR(255), trackName VARCHAR(255), timeSec INTEGER)''')
 
-conn.commit()
-
-cursor2 = conn.cursor()
-
-cursor2.execute('''CREATE TABLE IF NOT EXISTS Triplets 
+cursor.execute('''CREATE TABLE IF NOT EXISTS Triplets 
     (userId  INTEGER, trackId VARCHAR(255), listeningDate DATE)''')
 
 conn.commit()
@@ -21,35 +21,32 @@ reader = csv.reader(
     open("unique_tracks.txt"), delimiter=";")
 
 for row in reader:
-    values = [row[0], row[1], row[2], row[3], row[4]]
-    conn.execute('''INSERT INTO Music(performaceId, trackId, artistName, trackName, timeSec) VALUES (?, ?, ?, ?, ?)''',values)
+    conn.executemany('''INSERT INTO Music(performaceId, trackId, artistName, trackName, timeSec) VALUES (?, ?, ?, ?, ?)''',reader)
 
 reader = csv.reader(
     open("triplets_sample_20p.txt"), delimiter=";")
 
 for row in reader:
-    values = [row[0], row[1], row[2]]
-    conn.execute('''INSERT INTO Triplets (userId, trackId, listeningDate) VALUES (?, ?, ?)''',values)
+    conn.executemany('''INSERT INTO Triplets (userId, trackId, listeningDate) VALUES (?, ?, ?)''',reader)
 
-
-for row in cursor.execute('''SELECT * FROM Music LIMIT 1'''):
-    print(row)
-
-for row in cursor2.execute('''SELECT * FROM Triplets LIMIT 1'''):
-    print(row)
 
 print("\nNajpopularniejszy artysta:")
-for row in cursor2.execute('''SELECT artistName, COUNT(artistName) 
+for row in cursor.execute('''SELECT artistName, COUNT(artistName) 
     FROM Music 
     INNER JOIN Triplets ON Music.trackId = Triplets.trackId 
     GROUP BY Music.artistName
-    HAVING COUNT(artistName)>1 
     ORDER BY COUNT(artistName) DESC 
     LIMIT 1'''):
     print(row)
 
 print("\nLista 5 najpopularniejszych piosenek:")
-for row in cursor.execute('''SELECT trackName, COUNT(Triplets.trackId) FROM Music INNER JOIN Triplets ON Music.trackId = Triplets.trackId GROUP BY Triplets.trackID HAVING COUNT(Triplets.trackID)>1 ORDER BY COUNT(Triplets.trackID) DESC LIMIT 5'''):
+for row in cursor.execute('''SELECT trackName, 
+    COUNT(Triplets.trackId) 
+    FROM Music 
+    INNER JOIN Triplets ON Music.trackId = Triplets.trackId 
+    GROUP BY Triplets.trackID 
+    ORDER BY COUNT(Triplets.trackID) 
+    DESC LIMIT 5'''):
     print(row)
 
 
